02153743821f781bac284d0d60a86d88a31b6ad1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-08 13:28:26 +0530

    Initialized state for value in MUIDateRange component

--- src/components/MUIDateRange.jsx
+++ src/components/MUIDateRange.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { Box, TextField } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import React, { useState } from 'react';
 
 function MUIDateRange() {
+    const [value, setValue] = useState([null, null]);
+
     return (
         <div>MUIDateRange</div>
     )
