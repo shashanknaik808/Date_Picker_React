80030ebce9f614bbc589a46c76bb25e9e04b7b3a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-08 12:53:33 +0530

    Imported addDays, used useState, useRef, and initialized state in DateRangeComp component

--- src/components/DateRangeComp.jsx
+++ src/components/DateRangeComp.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { addDays } from 'date-fns';
+import React, { useRef, useState } from 'react';
 
 function DateRangeComp() {
+
+    const [range, setRange] = useState([{
+        startDate: new Date(),
+        endDate: addDays(new Date(), 7),
+        key: 'selection'
+    }]);
+
+    const [open, setOpen] = useState(false);
+    const refOne = useRef(null);
+    
     return (
         <div>DateRangeComp</div>
-    )
+    );
 }
 
 export default DateRangeComp;
