a7a57302b4feb83a059e609ac72b27b3803b099c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-08 12:29:36 +0530

    Integrate CalenderComp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Chip, Divider } from '@mui/material';
+import CalenderComp from './CalenderComp';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Divider>
         <Chip label="Calender" />
       </Divider>
+      <CalenderComp />
     </>
   );
 }
