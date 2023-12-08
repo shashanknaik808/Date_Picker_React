0854760553e20f35452db0123aa21f37e8277513
Shashank Naik <shashanknaik808@gmail.com>
2023-12-08 13:09:09 +0530

    Initialized state for range, open, and refOne in DateRangePickerComp component

--- src/components/DateRangePickerComp.jsx
+++ src/components/DateRangePickerComp.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 function DateRangePickerComp() {
+
+    const [range, setRange] = useState([{
+        startDate: new Date(),
+        endDate: addDays(new Date(), 7),
+        key: 'selection'
+    }]);
+
+    const [open, setOpen] = useState(false);
+    const refOne = useRef(null);
+
     return (
         <div>DateRangePickerComp</div>
     )
